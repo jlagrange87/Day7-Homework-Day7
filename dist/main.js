4f77d9a8fe2fd7f1a24d3b5fcd8b087b3f7dcda6
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-13 14:20:05 -0500

    Homework finished

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,27 @@
 function* guessingGame() {
-	console.log(yield game.say('Hey there!'));
-	console.log(yield game.say('Woah', 'This is cool'));
-	console.log(yield game.ask('Woah', 'This is cool'));
-	console.log(yield game.choose('Woah', 'This is cool', 'Hello world'));
-	console.log(yield game.say('That\'s all folks!', 'THE END'));
+	var randomnumber=Math.floor(Math.random()*100);
+	var guess = yield game.ask('Pick a number between 0 - 100');
+	yield game.say('You chose ' + guess);
+	yield game.say("Hmmmm, looks like...");
+	
+
+	while (parseInt(guess) !== randomnumber) {
+		
+		if (guess < randomnumber) {
+			yield game.say ("Your number is too low, pick again loser!");
+			guess = yield game.ask('Pick a number between 0 - 100... again.');
+			yield game.say("Hmmmm, looks like...");
+		}
+		else if (guess > randomnumber) {
+			yield game.say ("Your number is too high, quit being such a jerk!");
+			guess = yield game.ask('Pick a number between 0 - 100... again.');
+			yield game.say("Hmmmm, looks like...");
+		}
+		
+
+	}
+		yield game.say("YOU WIN! THE NUMBER WAS DEFINITELY " + randomnumber + "!");
+		yield game.say("YOU'RE THE BEST AROUND, NOTHINS GONNA EVER KEEP YOU DOWN!");
+		yield game.say("Seriously, thanks for playing!");
+		yield game.say("Goodbye!");
 }
