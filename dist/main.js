bc0870dcd708aed775a839e2669ea7f059841cf3
Joshua Lagrange <jlagrange87@gmail.com>
2015-05-13 16:47:19 -0500

    Hard mode complete

--- dist/main.js
+++ dist/main.js
@@ -1,27 +1,29 @@
 function* guessingGame() {
+	do {
 	var randomnumber=Math.floor(Math.random()*100);
+	console.log(randomnumber);
 	var guess = yield game.ask('Pick a number between 0 - 100');
-	yield game.say('You chose ' + guess);
+	yield game.say('You chose ' + guess + "!");
 	yield game.say("Hmmmm, looks like...");
 	
 
 	while (parseInt(guess) !== randomnumber) {
 		
 		if (guess < randomnumber) {
-			yield game.say ("Your number is too low, pick again loser!");
+			yield game.say ("Your number is too low, pick again!");
 			guess = yield game.ask('Pick a number between 0 - 100... again.');
 			yield game.say("Hmmmm, looks like...");
 		}
 		else if (guess > randomnumber) {
-			yield game.say ("Your number is too high, quit being such a jerk!");
+			yield game.say ("Your number is too high, try again champ!");
 			guess = yield game.ask('Pick a number between 0 - 100... again.');
 			yield game.say("Hmmmm, looks like...");
 		}
 		
 
 	}
-		yield game.say("YOU WIN! THE NUMBER WAS DEFINITELY " + randomnumber + "!");
-		yield game.say("YOU'RE THE BEST AROUND, NOTHINS GONNA EVER KEEP YOU DOWN!");
-		yield game.say("Seriously, thanks for playing!");
-		yield game.say("Goodbye!");
+
+		var cont = yield game.choose (randomnumber + " is correct! Would you like to keep playing?", "Yes", "No");
+	} while (cont == "Yes");
+	yield game.say ("Ok, thanks for playing!");
 }
